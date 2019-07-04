cb26f7dbf2de2de1ca4f09db5f5395f4176fa16f
Prashul Jain <jainprashul@gmail.com>
2019-07-04 16:37:52 +0530

    fetch event test and install banner pop up

--- sw.js
+++ sw.js
@@ -8,3 +8,9 @@ self.addEventListener("activate", evt=> {
     console.log("Service worker has been activated");
     
 })
+
+// fetch event
+self.addEventListener('fetch' , evt=>{
+    console.log("Page fetched", evt);
+
+})
