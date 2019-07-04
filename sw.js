a8e45f415aa9c6db82b3c60373b2b68526112217
Prashul Jain <jainprashul@gmail.com>
2019-07-04 23:38:46 +0530

    precaching assets and add cache files

--- sw.js
+++ sw.js
@@ -1,16 +1,36 @@
+const assets = [
+    './',
+    './index.html',
+    './css/materialize.css',
+    './css/styles.css',
+    './img/dish.png',
+    './js/app.js',
+    './js/materialize.js',
+    './js/ui.js',
+    './manifest.json'
+]
+
+const staticCacheName = 'site-static';
+
 // ServiceWorker install
 self.addEventListener('install', evt => {
     console.log("Service worker has been Installed");
-})
+});
 
 // activate worker
 self.addEventListener("activate", evt=> {
     console.log("Service worker has been activated");
     
-})
+});
 
 // fetch event
 self.addEventListener('fetch' , evt=>{
-    console.log("Page fetched", evt);
+    // console.log("Page fetched", evt);
+    evt.waitUntil(
+        caches.open(staticCacheName).then( cache => {
+            cache.addAll(assets);
+        })
+    );
 
-})
+
+});
